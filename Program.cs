675c3a7a6d8a123cd128700189318dc8a9ab23de
Павел Левицкий <creative-creative@outlook.com>
2022-09-05 13:00:06 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace string_analyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите сообщение");
+            string input_string = Console.ReadLine();
+            StringAnalyser(input_string);
+        }
+
+        private static void StringAnalyser(string input_string)
+        {
+            string analyzer_dict = "!\"#$%&'()*+,-./0123456789:;<=>?@[\\]^_`{|}~" +
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "абвгдежзийклмнопрстуфхцчшщъыьэюяАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            Dictionary<char, int> dic = new();
+            foreach (char ch in analyzer_dict)
+                dic.Add(ch, 0);
+            foreach (char ch in input_string)
+            {
+                if (analyzer_dict.Contains(ch.ToString()))
+                    dic[ch]++;
+            }
+            foreach (var pair in dic)
+                if (pair.Value > 0)
+                    Console.WriteLine("{0} {1}", pair.Key, pair.Value);
+
+        }
+    }
+}
